9ec552a8ffca3d4594f9f9923ead4835ddcd0f3e
hieunv-0908 <nn650370@gmail.com>
2024-12-25 15:01:18 +0700

    Add files via upload

---
+++ Sission21-Ex03-B24DTCN222.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+FILE *file;
+char input[100];
+file=fopen ("bt01.txt","a");
+if(file==NULL){
+	printf("file khong the mo duoc");
+	return 1;
+}
+printf("Nhap vao mot chuoi bat ki");
+fgets(input,100,stdin);
+
+fprintf(file,"%s",input);
+
+fclose;
+return 0;
+}
+
